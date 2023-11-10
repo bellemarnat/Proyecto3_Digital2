af37c442be07e72dbefa591466b0e441a7f315c3
Belle Marnat <isabelle.marnat2002@gmail.com>
2023-11-09 22:31:16 -0600

    primer intento giroscopio

--- Proyecto3_ESP32/src/main.cpp
+++ Proyecto3_ESP32/src/main.cpp
@@ -1,10 +1,134 @@
 /*----------------------------------------------------------------
 Universidad del Valle de Guatemala
 Electrónica Digital 2
-Profesor: Pablo Mazariegos y José Morales
+Profesores: Pablo Mazariegos y José Morales
 Proyecto 3 – Proyecto #3 I2C y NeoPixel
 Isabelle Marnat – 21020
 ----------------------------------------------------------------*/
 
 // ---------------- Librerias ----------------
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include "Wire.h"
+//#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_Sensor.h>
+
+// ---------------- Definiciones ----------------
+// //  NeoPixel
+// #define LED_PIN 15
+// #define LED_COUNT 16
+
+// Giroscopio - MPU6050
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+//Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+Adafruit_MPU6050 mpu;
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); 
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("No se pudo encontrar el chip MPU6050 ");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 encontrado!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  // Imprimir valores
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(500);
+}
