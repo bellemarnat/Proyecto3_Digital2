7b87b1b0a8498ab855aea691d58aade52362ca29
Belle Marnat <isabelle.marnat2002@gmail.com>
2023-11-09 19:47:15 -0600

    Creacion inicial ESP32

---
+++ Proyecto3_ESP32/src/main.cpp
@@ -0,0 +1,10 @@
+/*----------------------------------------------------------------
+Universidad del Valle de Guatemala
+Electrónica Digital 2
+Profesor: Pablo Mazariegos y José Morales
+Proyecto 3 – Proyecto #3 I2C y NeoPixel
+Isabelle Marnat – 21020
+----------------------------------------------------------------*/
+
+// ---------------- Librerias ----------------
+#include <Arduino.h>
